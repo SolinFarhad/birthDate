176b058b14fa6ada035c2dfe51b1d49a89870d06
SolinFarhad <77492339+SolinFarhad@users.noreply.github.com>
2021-01-15 16:04:24 +0300

    add files

---
+++ myBirthdate/myBirthdate/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace myBirthdate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Business Application Development
+            int year = DateTime.Today.Year;
+            int age = 21;
+            int myYearOfBirth = year - age;
+
+            Console.Write("My Year of birth is : ");
+            Console.WriteLine(myYearOfBirth);
+        }
+    }
+}
